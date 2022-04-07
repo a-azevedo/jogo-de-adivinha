25d7a734ed39c0b3ab436569de6ca546a5dab6b3
a-azevedo <aelmajan.azevedo@gmail.com>
2022-04-07 17:22:17 -0300

    Criado um método para cada processo

--- adivinha-game.js
+++ adivinha-game.js
@@ -1,25 +1,42 @@
-var numberToBeGuessed; 
+var numberToBeGuessed;
 
-var guess;//parseInt(prompt("Já pensei! Qual número você acha que é?"));
+var guess;
 
-function sortNumber(){
+function playerGuess(id) {
+    sortNumber();
+    guess = document.getElementById(id);
+    guess = parseInt(id);
+    console.log("Player guess " + guess)
+    checkResult();
+    return guess;
+}
+
+function sortNumber() {
     numberToBeGuessed = Math.round(Math.random() * 10);
-    if(numberToBeGuessed == 0){
+    if (numberToBeGuessed == 0) {
         numberToBeGuessed = Math.round(Math.random() * 10);
     }
-}
-function skipALine(){
-    document.write("<br>")
-}
-function showMessage(message){
-    document.write(message);
-    skipALine();
-}
-sortNumber();
-console.log(numberToBeGuessed);
-if (guess == numberToBeGuessed){
-    showMessage("Nossa! Você acertou mesmo! Eu pensei no número " + numberToBeGuessed);
-} else {
-    showMessage("Não foi dessa vez. Eu pensei no " + numberToBeGuessed);
+    console.log("IA choose " + numberToBeGuessed);
+    return numberToBeGuessed;
 }
 
+function skipALine() {
+    //document.write("<br>")
+}
+
+function showMessage(message) {
+    document.write(message);    
+    skipALine();
+}
+function checkResult(){
+    console.log(guess);
+    console.log(numberToBeGuessed);
+    if (guess == numberToBeGuessed) {
+        var resultado  = document.getElementById("gs");
+        resultado.value = "Nossa! Você acertou mesmo! Eu pensei no número " + numberToBeGuessed;
+        
+    } else {
+        
+       document.getElementById("gs").value = "dsadasdas"
+    }
+}
